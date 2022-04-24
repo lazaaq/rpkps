2c5be11c8e609ca403836b0e4dd1aa5b16bd31d2
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-04-24 22:03:13 +0700

    Add Relationship Between Course and StudyProgram

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,4 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Course extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function studyProgram()
+    {
+        return $this->belongsTo(StudyProgram::class);
+    }
 }
