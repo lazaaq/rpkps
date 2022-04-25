71af07eb8acf907830ed1f6e34ed8a683f286347
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-04-25 15:26:17 +0700

    Add Model, Migration, Factory, and Seeder for Learning Goal Table

---
+++ database/factories/LearningGoalFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\LearningGoal>
+ */
+class LearningGoalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'code' => $this->faker->word(),
+            'component' => $this->faker->word(),
+            'value' => $this->faker->paragraph(),
+        ];
+    }
+}
