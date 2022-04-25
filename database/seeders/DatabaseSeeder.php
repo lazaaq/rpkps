50f06fc6a2f9e1ecd27da4bec3f3e05604830c8d
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-04-25 15:23:22 +0700

    Add Model, Migration, Factory, and Seeder for Graduate Profile Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Semester;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CourseSeeder::class,
             StudyProgramSeeder::class,
-            SemesterSeeder::class
+            SemesterSeeder::class,
         ]);
     }
 }
