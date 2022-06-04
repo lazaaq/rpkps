6bcc8337c5cf1428263a47dc5cea338003a06683
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-06-04 11:05:10 +0700

    Delete softDeletes in lecturer table

--- database/migrations/2022_04_26_044851_create_lecturers_table.php
+++ database/migrations/2022_04_26_044851_create_lecturers_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('nip');
             $table->string('email');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
